f636f041942c8d964280325924f9dda0d98c7e67
ashwath.bharadwaj <ashwath.bharadwaj@73strings.com>
2022-04-10 20:25:08 +0530

    test

--- projects/xui/src/environments/environment.prod.ts
+++ projects/xui/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 const packageJson = require('../../../../package.json');
 
 export const environment = {
-  appName: 'Angular Ngrx Material Starter',
+  appName: 'XUI',
   envName: 'PROD',
   production: true,
   test: false,
-  i18nPrefix: '/xui',
+  i18nPrefix: '',
   versions: {
     app: packageJson.version,
     angular: packageJson.dependencies['@angular/core'],
