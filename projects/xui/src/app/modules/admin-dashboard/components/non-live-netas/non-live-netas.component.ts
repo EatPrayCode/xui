6ec3720cd1f3b77c87cd3a43ee51636fb942d937
ashwath.bharadwaj <ashwath.bharadwaj@73strings.com>
2022-04-21 16:36:53 +0530

    test

--- projects/xui/src/app/modules/admin-dashboard/components/non-live-netas/non-live-netas.component.ts
+++ projects/xui/src/app/modules/admin-dashboard/components/non-live-netas/non-live-netas.component.ts
@@ -3,6 +3,16 @@ import { of } from 'rxjs';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Airtable, Base } from 'ngx-airtable';
 
+import { from, Observable } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+import { TransferState } from '@angular/platform-browser';
+import { traceUntilFirst } from '@angular/fire/performance';
+import { getDownloadURL, ref, Storage } from '@angular/fire/storage';
+import { keepUnstableUntilFirst } from '@angular/fire';
+
+const TRANSPARENT_PNG
+  = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
 @Component({
   selector: 'app-non-live-netas',
   templateUrl: './non-live-netas.component.html',
@@ -11,15 +21,25 @@ import { Airtable, Base } from 'ngx-airtable';
 })
 export class NonLiveNetasComponent implements OnInit {
 
-  nonLiveNetas$: any = of([])
+  nonLiveNetas$: any = of([]);
+  public readonly downloadUrl$: Observable<string>;
 
-  constructor(private _airtable: Airtable) {
+  constructor(
+    private _airtable: Airtable,
+    storage: Storage
+  ) {
+    const icon = ref(storage, 'admin-uploads/aypqssti.png');
+    this.downloadUrl$ = from(getDownloadURL(icon)).pipe(
+      keepUnstableUntilFirst,
+      traceUntilFirst('storage'),
+      startWith(TRANSPARENT_PNG),
+    );
     const base: Base = this._airtable
       .base('app0blxbqN1rHDD24');
     this.nonLiveNetas$ = base.table({
       tableId: 'tblL0z0AtzG8d5OWG'
     }).select({ maxRecords: 10 })
-      .firstPage().pipe(tap(res=>{
+      .firstPage().pipe(tap(res => {
         console.log(res);
       }));
   }
