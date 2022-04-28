cf224b630e758bd862ab486fb143b80313ddba71
ashwath.bharadwaj <ashwath.bharadwaj@73strings.com>
2022-04-29 00:46:50 +0530

    testtest

--- projects/xui/src/app/modules/home/home.module.ts
+++ projects/xui/src/app/modules/home/home.module.ts
@@ -1,10 +1,18 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [HomeComponent]
 })
-export class SetupModule {}
+export class SetupModule { }
